e1fb3f74ebedb5bb33ae0192cbdbd4db8f176180
deriabin_85 <deriabin_85@mail.ru>
2024-05-01 11:07:12 +0300

    Добавил поиск на главную страницу, добавил описание сайта на странице about.

--- freelance/pages/urls.py
+++ freelance/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, SearchResultListView
 
 
 urlpatterns = [
  path('', HomePageView.as_view(), name='home'),
- path('about', AboutPageView.as_view(), name='about')
+ path('about/', AboutPageView.as_view(), name='about'),
+ path('search/', SearchResultListView.as_view(), name='search_results')
  ]
--- freelance/pages/views.py
+++ freelance/pages/views.py
@@ -1,4 +1,9 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+
+UserModel = get_user_model()
 
 
 class HomePageView(TemplateView):
@@ -7,3 +12,17 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class SearchResultListView(ListView):
+    model = UserModel
+    context_object_name = 'results'
+    template_name = 'search_results.html'
+
+    def get_queryset(self):
+        querry = self.request.GET.get('q')
+        return UserModel.objects.filter(
+            Q(experience__icontains=querry) |
+            Q(first_name__icontains=querry) |
+            Q(last_name__icontains=querry)
+        )
